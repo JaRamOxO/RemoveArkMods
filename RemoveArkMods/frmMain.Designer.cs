b613953a5bb4b97040a9edcfcb9728297cbae256
JaRam <gitjaram@nerdene.com>
2017-06-27 00:44:06 +0200

    Add project files.

---
+++ RemoveArkMods/frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace RemoveArkMods
+{
+    sealed partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeleteMod = new System.Windows.Forms.Button();
+            this.txtModId = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.fbdArkInstallDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnArkInstallDir = new System.Windows.Forms.Button();
+            this.lblArkInstallDir = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblModSteamInfo = new System.Windows.Forms.Label();
+            this.lblObs = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDeleteMod
+            // 
+            this.btnDeleteMod.Enabled = false;
+            this.btnDeleteMod.Location = new System.Drawing.Point(217, 92);
+            this.btnDeleteMod.Name = "btnDeleteMod";
+            this.btnDeleteMod.Size = new System.Drawing.Size(75, 24);
+            this.btnDeleteMod.TabIndex = 0;
+            this.btnDeleteMod.Text = "Delete mod";
+            this.btnDeleteMod.UseVisualStyleBackColor = true;
+            this.btnDeleteMod.Click += new System.EventHandler(this.btnDeleteMod_Click);
+            // 
+            // txtModId
+            // 
+            this.txtModId.Location = new System.Drawing.Point(33, 93);
+            this.txtModId.Name = "txtModId";
+            this.txtModId.Size = new System.Drawing.Size(178, 20);
+            this.txtModId.TabIndex = 2;
+            this.txtModId.TextChanged += new System.EventHandler(this.txtModId_TextChanged);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(30, 142);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(62, 13);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Information:";
+            // 
+            // fbdArkInstallDir
+            // 
+            this.fbdArkInstallDir.Description = "Find the ARK folder, under steam directory";
+            this.fbdArkInstallDir.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // btnArkInstallDir
+            // 
+            this.btnArkInstallDir.Location = new System.Drawing.Point(32, 44);
+            this.btnArkInstallDir.Name = "btnArkInstallDir";
+            this.btnArkInstallDir.Size = new System.Drawing.Size(75, 23);
+            this.btnArkInstallDir.TabIndex = 3;
+            this.btnArkInstallDir.Text = "Browse...";
+            this.btnArkInstallDir.UseVisualStyleBackColor = true;
+            this.btnArkInstallDir.Click += new System.EventHandler(this.btnArkInstallDir_Click);
+            // 
+            // lblArkInstallDir
+            // 
+            this.lblArkInstallDir.AutoSize = true;
+            this.lblArkInstallDir.Location = new System.Drawing.Point(113, 49);
+            this.lblArkInstallDir.Name = "lblArkInstallDir";
+            this.lblArkInstallDir.Size = new System.Drawing.Size(168, 13);
+            this.lblArkInstallDir.TabIndex = 1;
+            this.lblArkInstallDir.Text = "Browse to set ARK install directory";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ModId:";
+            // 
+            // lblModSteamInfo
+            // 
+            this.lblModSteamInfo.AutoSize = true;
+            this.lblModSteamInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModSteamInfo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblModSteamInfo.Location = new System.Drawing.Point(30, 120);
+            this.lblModSteamInfo.Name = "lblModSteamInfo";
+            this.lblModSteamInfo.Size = new System.Drawing.Size(98, 13);
+            this.lblModSteamInfo.TabIndex = 4;
+            this.lblModSteamInfo.Text = "Steam mod info:";
+            // 
+            // lblObs
+            // 
+            this.lblObs.AutoSize = true;
+            this.lblObs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObs.ForeColor = System.Drawing.Color.Red;
+            this.lblObs.Location = new System.Drawing.Point(30, 13);
+            this.lblObs.Name = "lblObs";
+            this.lblObs.Size = new System.Drawing.Size(360, 13);
+            this.lblObs.TabIndex = 5;
+            this.lblObs.Text = "You should unsubscribe the mod in steam before you delete it!";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 271);
+            this.Controls.Add(this.lblObs);
+            this.Controls.Add(this.lblModSteamInfo);
+            this.Controls.Add(this.btnArkInstallDir);
+            this.Controls.Add(this.txtModId);
+            this.Controls.Add(this.lblArkInstallDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnDeleteMod);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Delete ARK mod";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeleteMod;
+        private System.Windows.Forms.TextBox txtModId;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.FolderBrowserDialog fbdArkInstallDir;
+        private System.Windows.Forms.Button btnArkInstallDir;
+        private System.Windows.Forms.Label lblArkInstallDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblModSteamInfo;
+        private System.Windows.Forms.Label lblObs;
+    }
+}
+
---
+++ RemoveArkMods/frmMain.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Principal;
+
+namespace RemoveArkMods
+{
+    public sealed partial class frmMain : Form
+    {
+        private string _arkInstallDir = "";
+        private bool _isValidModId = false;
+        private bool _isValidArkDir = false;
+
+        private string _delFolder1 = "";
+        private string _delFile = "";
+        private string _delFolder2 = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            if (IsUserAdministrator())
+            {
+                Text = @"Delete ARK mod (Administrator)";
+            }
+        }
+
+        public bool IsUserAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+
+        private void btnArkInstallDir_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbdArkInstallDir.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _arkInstallDir = fbdArkInstallDir.SelectedPath;
+                lblArkInstallDir.Text = _arkInstallDir;
+                if (!IsArkDirCorrect())
+                {
+                    MessageBox.Show(this, @"Choosen ARK install directory is not correct!", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+        }
+
+        private bool IsArkDirCorrect()
+        {
+            DirectoryInfo di = new DirectoryInfo(_arkInstallDir);
+            di.EnumerateDirectories();
+            if (di.Parent != null && di.Parent.ToString() == "common")
+            {
+                if (di.Parent.Parent != null && di.Parent.Parent.ToString() == "steamapps")
+                {
+                    _isValidArkDir = true;
+                    btnDeleteMod.Enabled = IsAllValid();
+                    SetInformation(!IsAllValid());
+                }
+                else _isValidArkDir = false;
+            }
+            return _isValidArkDir;
+        }
+
+        private bool IsAllValid()
+        {
+            return _isValidArkDir && _isValidModId;
+        }
+
+        private string GetModNameFromSteam(string modid)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri("http://steamcommunity.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/html"));
+
+                var response = client.GetAsync($"sharedfiles/filedetails/?id={modid}").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result))
+                    {
+                        var tempHtml = reader.ReadToEnd();
+                        int start = tempHtml.IndexOf("<title>", StringComparison.Ordinal) + 24;
+                        int len = tempHtml.IndexOf("</title>", StringComparison.Ordinal) - start;
+
+                        return tempHtml.Substring(start, len).Replace("&amp;", "&").Replace(": Error", " Error: Hidden, deleted mod");
+                    }
+                }
+                else
+                {
+                    return "error";
+                    //throw new System.Net.WebException("Something went wrong with getting the modname for " + modid);
+                }
+            }
+        }
+
+        private void txtModId_TextChanged(object sender, EventArgs e)
+        {
+            if (txtModId.Text.Length >= 9)
+            {
+                string temp = GetModNameFromSteam(txtModId.Text);
+                if (temp.ToLower().Contains("error")) return;
+                lblModSteamInfo.Text = $@"Steam mod info: {temp}";
+                _isValidModId = true;
+                btnDeleteMod.Enabled = IsAllValid();
+                SetInformation(!IsAllValid());
+            }
+            else
+            {
+                _isValidModId = false;
+                btnDeleteMod.Enabled = IsAllValid();
+                SetInformation(!IsAllValid());
+                lblModSteamInfo.Text = @"Steam mod info:";
+            } 
+        }
+
+        private void SetInformation(bool reset)
+        {
+            if (reset)
+            {
+                lblInfo.Text = @"Information:";
+            }
+            else
+            {
+                var di = new DirectoryInfo(_arkInstallDir);
+                _delFolder1 = $"{di.FullName}\\ShooterGame\\Content\\Mods\\{txtModId.Text}";
+                _delFile = $"{di.FullName}\\ShooterGame\\Content\\Mods\\{txtModId.Text}.mod";
+                _delFolder2 = $"{di.Parent.Parent.FullName}\\workshop\\content\\346110\\{txtModId.Text}";
+                lblInfo.Text = $"Information:\nTo be deleted...\n{_delFolder1}\n{_delFile}\n{_delFolder2}";
+            }
+        }
+
+        private void DeletedReset()
+        {
+            string temp = $"Information: \n\n All folders/files for mod {txtModId.Text} is now deleted.";
+            txtModId.Text = "";
+            lblInfo.Text = temp;
+        }
+
+        private void btnDeleteMod_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(_delFolder1)) Directory.Delete(_delFolder1, true);
+                if (File.Exists(_delFile)) File.Delete(_delFile);
+                if (Directory.Exists(_delFolder2)) Directory.Delete(_delFolder2, true);
+                DeletedReset();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}\n\n{ex.InnerException}");
+            }
+            
+        }
+    }
+}
